c43ffdb5335e0e45b6eed5930d835de22e31331a
olga-kuriatnyk <olhak@uw.edu>
2021-07-29 23:01:45 -0700

    build custom hook & work with the API

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 
 import Header from '../Header';
 import Form from '../Form';
-// import Error from '../Error';
+import Error from '../Error';
 import Loader from '../Loader';
 import Forecast from '../Forecast';
 
@@ -11,27 +11,27 @@ import useForecast from '../../hooks/useForecast';
 import styles from './Page.module.css';
 
 const Page = () => {
-    //  const { isError, isLoading, forecast, submitRequest } = useForecast();
-    const { isLoading, forecast, submitRequest } = useForecast();
+    const { isError, isLoading, forecast, submitRequest } = useForecast();
 
     const onSubmit = value => {
         submitRequest(value);
-    }
+    };
 
     return (
         <Fragment>
             <Header />
-            <div className={styles.box}>
-                {/* From */}
-                {/* {isLoading && <Form submitSearch={onSubmit} />} */}
-                <Form submitSearch={onSubmit} />
-                {/* Error */}
-                {/* {isError && <Error message={isError}/>} */}
-                {/* Loader */}
-                {isLoading && <Loader />}
-            </div>
+            {!forecast && (
+                <div className={`${styles.box} position-relative`}>
+                    {/* Form */}
+                    {!isLoading && <Form submitSearch={onSubmit} />}
+                    {/* Error */}
+                    {isError && <Error message={isError} />}
+                    {/* Loader */}
+                    {isLoading && <Loader />}
+                </div>
+            )}
             {/* Forecast */}
-            {forecast && <Forecast />}
+            {forecast && <Forecast forecast={forecast} />}
         </Fragment>
     );
 };
